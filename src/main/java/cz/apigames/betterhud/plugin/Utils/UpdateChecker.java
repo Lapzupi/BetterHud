d7c8e0886b8c3c397dc9cb9eac819e574d736550
Omer Oreg <sarhatabaot1@gmail.com>
2022-05-16 22:35:49 +0300

    Minor refactoring

--- src/main/java/cz/apigames/betterhud/plugin/Utils/UpdateChecker.java
+++ src/main/java/cz/apigames/betterhud/plugin/Utils/UpdateChecker.java
@@ -5,6 +5,7 @@ import cz.apigames.betterhud.api.utils.MessageUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
@@ -14,35 +15,36 @@ import java.net.URL;
 
 public class UpdateChecker implements Listener {
 
-    private static String api = "https://api.spigotmc.org/legacy/update.php?resource=";
-    private static String id = "84180";
+    private static final String API = "https://api.spigotmc.org/legacy/update.php?resource=";
+    private static final String ID = "84180";
 
     private static boolean newUpdate;
     private static String latestVersion;
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        if(event.getPlayer().hasPermission("betterhud.update.notify")) {
-            if(newUpdate) {
-                event.getPlayer().sendMessage(MessageUtils.colorize(" &eBetterHud &8» &7New version available! Your version: &e"+BetterHud.getVersion()+" &7| Latest version: &6"+latestVersion));
-                event.getPlayer().sendMessage(MessageUtils.colorize(" &eDownload here &8» &6https://spigotmc.org/resource/84180"));
-            }
+    public void onJoin(@NotNull PlayerJoinEvent event) {
+        if (event.getPlayer().hasPermission("betterhud.update.notify")
+                && newUpdate) {
+
+            event.getPlayer().sendMessage(MessageUtils.colorize(" &eBetterHud &8» &7New version available! Your version: &e" + BetterHud.getVersion() + " &7| Latest version: &6" + latestVersion));
+            event.getPlayer().sendMessage(MessageUtils.colorize(" &eDownload here &8» &6https://spigotmc.org/resource/84180"));
+
         }
     }
 
     public static void checkUpdate() {
         try {
-            HttpsURLConnection connection = (HttpsURLConnection) new URL(api + id).openConnection();
+            HttpsURLConnection connection = (HttpsURLConnection) new URL(API + ID).openConnection();
             connection.setRequestMethod("GET");
             latestVersion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
 
-            if(BetterHud.getVersion().contains("SNAPSHOT")) {
+            if (BetterHud.getVersion().contains("SNAPSHOT")) {
                 newUpdate = false;
                 BetterHud.sendMessageToConsole("You are using a SNAPSHOT version, which means, that some functions can be unstable. &cUse this version on production server at your own risk.");
                 return;
             }
 
-            if(!BetterHud.getVersion().equalsIgnoreCase(latestVersion)) {
+            if (!BetterHud.getVersion().equalsIgnoreCase(latestVersion)) {
                 newUpdate = true;
                 return;
             }
