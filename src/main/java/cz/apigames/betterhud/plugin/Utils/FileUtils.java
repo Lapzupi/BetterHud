e2343b288c07d544eadaaf96e8690881545c80f5
Omer Oreg <sarhatabaot1@gmail.com>
2022-05-16 22:34:53 +0300

    Now using try with resources

--- src/main/java/cz/apigames/betterhud/plugin/Utils/FileUtils.java
+++ src/main/java/cz/apigames/betterhud/plugin/Utils/FileUtils.java
@@ -1,6 +1,7 @@
 package cz.apigames.betterhud.plugin.utils;
 
 import cz.apigames.betterhud.BetterHud;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,15 +10,16 @@ import java.security.MessageDigest;
 
 public class FileUtils {
 
-    public static String checksum(File file) {
+    public static @NotNull String checksum(File file) {
         try {
             MessageDigest digest = MessageDigest.getInstance("MD5");
-            FileInputStream fis = new FileInputStream(file);
-            byte[] byteArray = new byte[1024];
-            int bytesCount = 0;
-            while ((bytesCount = fis.read(byteArray)) != -1)
-                digest.update(byteArray, 0, bytesCount);
-            fis.close();
+            try (FileInputStream fis = new FileInputStream(file)) {
+                byte[] byteArray = new byte[1024];
+                int bytesCount;
+                while ((bytesCount = fis.read(byteArray)) != -1)
+                    digest.update(byteArray, 0, bytesCount);
+            }
+
             byte[] bytes = digest.digest();
             StringBuilder sb = new StringBuilder();
             for (byte aByte : bytes)
@@ -25,8 +27,9 @@ public class FileUtils {
                         Integer.toString((aByte & 0xFF) + 256, 16)
                                 .substring(1));
             return sb.toString();
-        } catch (IOException |java.security.NoSuchAlgorithmException e) {
-            BetterHud.error("Failed to get File checksum! Path: "+file.getAbsolutePath(), e);
+
+        } catch (IOException | java.security.NoSuchAlgorithmException e) {
+            BetterHud.error("Failed to get File checksum! Path: " + file.getAbsolutePath(), e);
             return "";
         }
     }
